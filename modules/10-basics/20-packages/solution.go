7ca4002f8298c2f6c599499dc6186776d1b30d26
Ivan Lemeshev <vanlmshv@gmail.com>
2020-03-26 22:50:20 +0300

    Fix linter errors

--- modules/10-basics/20-packages/solution.go
+++ modules/10-basics/20-packages/solution.go
@@ -2,6 +2,7 @@ package solution
 
 import "fmt"
 
+// Hello prints the strins "Hello, World!" on the screen.
 func Hello() {
 	fmt.Print("Hello, World!")
 }
--- modules/10-basics/30-functions/solution.go
+++ modules/10-basics/30-functions/solution.go
@@ -3,8 +3,10 @@ package solution
 import "fmt"
 
 // BEGIN
+// PrintTwice prints the given value on the screen twice.
 func PrintTwice(value string) {
 	fmt.Println(value)
 	fmt.Print(value)
 }
+
 // END
