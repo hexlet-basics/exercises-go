61730a95311e8311ef30350641135d518dd90d20
Vlad Gukasov <v.gukasov@space307.com>
2021-09-01 10:02:58 +0300

    add lesson 5: functions

---
+++ modules/10-basics/50-funcs/solution.go
@@ -0,0 +1,12 @@
+package solution
+
+import "strconv"
+
+// BEGIN
+
+// IntToString converts i number to a string representation.
+func IntToString(i int) string {
+	return strconv.Itoa(i)
+}
+
+// END
---
+++ modules/10-basics/50-funcs/solution_test.go
@@ -0,0 +1,36 @@
+package solution_test
+
+import (
+	"strconv"
+	"testing"
+
+	solution "exercises-go/modules/10-basics/50-funcs"
+)
+
+func TestIntToString(t *testing.T) {
+	tests := []struct {
+		input int
+		want  string
+	}{
+		{
+			input: 0,
+			want:  "0",
+		},
+		{
+			input: -42,
+			want:  "-42",
+		},
+		{
+			input: 100500,
+			want:  "100500",
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := solution.IntToString(tt.input); got != tt.want {
+				t.Errorf("IntToString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
