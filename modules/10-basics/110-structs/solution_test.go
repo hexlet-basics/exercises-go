ed050984ec55487ee46d26d36e4bab752cf1ba05
Vlad Gukasov <forpelevin@gmail.com>
2021-09-03 22:20:39 +0300

    add lesson 11: structs (#35)
    Co-authored-by: Vlad Gukasov <v.gukasov@space307.com>

---
+++ modules/10-basics/110-structs/solution_test.go
@@ -0,0 +1,45 @@
+package solution_test
+
+import (
+	"testing"
+
+	solution "exercises-go/modules/10-basics/110-structs"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMinInt(t *testing.T) {
+	a := assert.New(t)
+	a.Equal("invalid request", solution.Validate(solution.UserCreateRequest{
+		FirstName: "",
+		Age:       0,
+	}))
+	a.Equal("invalid request", solution.Validate(solution.UserCreateRequest{
+		FirstName: "John",
+		Age:       -5,
+	}))
+	a.Equal("invalid request", solution.Validate(solution.UserCreateRequest{
+		FirstName: "Andy",
+		Age:       0,
+	}))
+	a.Equal("invalid request", solution.Validate(solution.UserCreateRequest{
+		FirstName: "Karl",
+		Age:       151,
+	}))
+	a.Equal("invalid request", solution.Validate(solution.UserCreateRequest{
+		FirstName: "",
+		Age:       5,
+	}))
+	a.Equal("invalid request", solution.Validate(solution.UserCreateRequest{
+		FirstName: " Henry",
+		Age:       15,
+	}))
+	a.Equal("", solution.Validate(solution.UserCreateRequest{
+		FirstName: "John",
+		Age:       150,
+	}))
+	a.Equal("", solution.Validate(solution.UserCreateRequest{
+		FirstName: "Susan",
+		Age:       30,
+	}))
+}
