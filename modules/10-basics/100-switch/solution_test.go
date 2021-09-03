7aa877bac289e819be8d186dea886ade5de0715e
Vlad Gukasov <v.gukasov@space307.com>
2021-09-03 10:43:25 +0300

    add lesson 10: switch

---
+++ modules/10-basics/100-switch/solution_test.go
@@ -0,0 +1,17 @@
+package solution_test
+
+import (
+	"testing"
+
+	solution "exercises-go/modules/10-basics/100-switch"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMinInt(t *testing.T) {
+	a := assert.New(t)
+	a.Equal("-hello-world-", solution.ModifySpaces(" hello world ", "dash"))
+	a.Equal("_how_r_u_doing_", solution.ModifySpaces(" how r u doing ", "underscore"))
+	a.Equal("*great*", solution.ModifySpaces(" great ", ""))
+	a.Equal("*nice*", solution.ModifySpaces(" nice ", "unknwn"))
+}
