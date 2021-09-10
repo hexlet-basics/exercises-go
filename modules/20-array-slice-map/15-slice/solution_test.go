5023bb40e189a6660ce62668dc5662f7ea427cca
Vlad Gukasov <v.gukasov@space307.com>
2021-09-10 09:04:11 +0300

    add lesson 2-2: slices

---
+++ modules/20-array-slice-map/15-slice/solution_test.go
@@ -0,0 +1,26 @@
+package solution_test
+
+import (
+	"sort"
+	"testing"
+
+	solution "exercises-go/modules/20-array-slice-map/15-slice"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSafeWrite(t *testing.T) {
+	a := assert.New(t)
+	equalSlices(a, []int{1, 2}, solution.Remove([]int{1, 2}, -1))
+	equalSlices(a, []int{1, 2}, solution.Remove([]int{1, 2}, -5))
+	equalSlices(a, []int{2, 3}, solution.Remove([]int{1, 2, 3}, 0))
+	equalSlices(a, []int{1, 2}, solution.Remove([]int{1, 2, 3}, 2))
+	equalSlices(a, []int{1, 2, 3}, solution.Remove([]int{1, 2, 3}, 3))
+	equalSlices(a, []int{1, 2, 3}, solution.Remove([]int{1, 2, 3}, 5))
+}
+
+func equalSlices(a *assert.Assertions, expected, actual []int) {
+	sort.Ints(expected)
+	sort.Ints(actual)
+	a.Equal(expected, actual)
+}
