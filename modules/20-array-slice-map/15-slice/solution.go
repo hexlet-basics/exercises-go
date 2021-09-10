86e49b0d79f1c2216dae3d2641af8213e31a15a9
Vlad Gukasov <v.gukasov@space307.com>
2021-09-10 09:04:11 +0300

    add lesson 2-2: slices

---
+++ modules/20-array-slice-map/15-slice/solution.go
@@ -0,0 +1,17 @@
+package solution
+
+
+// BEGIN
+
+// Remove removes an item from the nums slice by index i.
+func Remove(nums []int, i int) []int {
+	if i < 0 || i > len(nums) - 1 {
+		return nums
+	}
+
+	nums[i] = nums[len(nums) - 1]
+
+	return nums[:len(nums) - 1]
+}
+
+// END
