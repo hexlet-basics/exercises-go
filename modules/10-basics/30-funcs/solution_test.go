36d3db236024507d2929586b82db30dbea20db9a
Vlad Gukasov <v.gukasov@space307.com>
2021-09-14 10:20:37 +0300

    Lesson texts fixes. File permissions fix. Add description for the 1st module

--- modules/10-basics/30-funcs/solution_test.go
+++ modules/10-basics/30-funcs/solution_test.go
@@ -1,34 +1,13 @@
 package solution
 
 import (
-	"strconv"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestIntToString(t *testing.T) {
-	tests := []struct {
-		input int
-		want  string
-	}{
-		{
-			input: 0,
-			want:  "0",
-		},
-		{
-			input: -42,
-			want:  "-42",
-		},
-		{
-			input: 100500,
-			want:  "100500",
-		},
-	}
-
-	for i, tt := range tests {
-		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			if got := IntToString(tt.input); got != tt.want {
-				t.Errorf("IntToString() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	a := assert.New(t)
+	a.Equal("0", IntToString(0))
+	a.Equal("-42", IntToString(-42))
+	a.Equal("100500", IntToString(100500))
 }
--- modules/20-array-slice-map/10-array/solution.go
+++ modules/20-array-slice-map/10-array/solution.go
@@ -2,15 +2,15 @@ package solution
 
 // BEGIN
 
-// SafeWrite writes a value val in the array arr if the index i is in the array's boundaries.
-func SafeWrite(arr [5]int, i, val int) [5]int {
-	if i < 0 || i >= len(arr) {
-		return arr
+// SafeWrite writes a value val in the array nums if the index i is in the array's boundaries.
+func SafeWrite(nums [5]int, i, val int) [5]int {
+	if i < 0 || i >= len(nums) {
+		return nums
 	}
 
-	arr[i] = val
+	nums[i] = val
 
-	return arr
+	return nums
 }
 
 // END
