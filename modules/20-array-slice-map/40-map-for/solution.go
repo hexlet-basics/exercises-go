9d069033587562a1066a26caa3d69675096f14e1
anatoliy.poloz <anatoliy.poloz@hey.com>
2021-10-28 02:08:31 +0300

    Update solution.go
    maybe my solution is not optimal in terms of memory consumption. Or maybe not in the style of the Go language. But it seems shorter and clearer.

--- modules/20-array-slice-map/40-map-for/solution.go
+++ modules/20-array-slice-map/40-map-for/solution.go
@@ -1,54 +1,18 @@
 package solution
 
 // BEGIN
-
-type popularityIndex struct {
-	firstIndex int
-	popularity int
-}
-
-// MostPopularWord returns most popular word from the words slice.
-// If there are multiple popular words it returns the first one depending on the words slice order.
 func MostPopularWord(words []string) string {
-	wordsPopularity := make(map[string]popularityIndex)
+	wordsCount := make(map[string]int, 0)
+	result := ""
+	max := 0
 
-	for i, w := range words {
-		pi, ok := wordsPopularity[w]
-		if !ok {
-			wordsPopularity[w] = popularityIndex{
-				firstIndex: i,
-				popularity: 1,
-			}
-			continue
-		}
-
-		pi.popularity++
-
-		wordsPopularity[w] = pi
-	}
-
-	var (
-		mostPopWord     string
-		firstPopWordIdx int
-		maxPop          int
-	)
-
-	for w, pi := range wordsPopularity {
-		if pi.popularity > maxPop {
-			maxPop = pi.popularity
-			firstPopWordIdx = pi.firstIndex
-			mostPopWord = w
-			continue
-		}
-
-		if pi.popularity == maxPop && pi.firstIndex < firstPopWordIdx {
-			maxPop = pi.popularity
-			firstPopWordIdx = pi.firstIndex
-			mostPopWord = w
+	for _, word := range words {
+		wordsCount[word]++
+		if wordsCount[word] > max {
+			max = wordsCount[word]
+			result = word
 		}
 	}
-
-	return mostPopWord
+	return result
 }
-
 // END
