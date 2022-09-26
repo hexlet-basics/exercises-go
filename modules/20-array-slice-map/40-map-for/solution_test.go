eef5a9c6383def4cfc164bc2b673e8b63a8dde19
Roman Ashikov <rmn.shkv@gmail.com>
2022-09-26 23:14:05 +0700

    (20/40) remove duplicate test case

--- modules/20-array-slice-map/40-map-for/solution_test.go
+++ modules/20-array-slice-map/40-map-for/solution_test.go
@@ -11,6 +11,5 @@ func TestMostPopularWord(t *testing.T) {
 	a.Equal("hello", MostPopularWord([]string{"hello", "world", "hello"}))
 	a.Equal("world", MostPopularWord([]string{"hello", "world", "hello", "world", "world"}))
 	a.Equal("one", MostPopularWord([]string{"one", "two", "three", "four", "five"}))
-	a.Equal("one", MostPopularWord([]string{"one", "two", "three", "four", "five"}))
 	a.Equal("c", MostPopularWord([]string{"a", "b", "c", "c", "d", "e", "e", "d"}))
 }
