dba29cb02186f4bfacc7cf95b7711df71a134b07
Roman Ashikov <rmn.shkv@gmail.com>
2022-09-28 19:16:31 +0700

    (30/15) improve solution

--- modules/30-strings/15-strings-for/solution.go
+++ modules/30-strings/15-strings-for/solution.go
@@ -1,19 +1,17 @@
 package solution
 
 // BEGIN
-
-// shiftASCII shifts each byte in the string s and if there is an overflow of ASCII it takes a new code by module of 256.
 func shiftASCII(s string, step int) string {
 	if step == 0 {
 		return s
 	}
 
-	shifted := make([]byte, len(s))
+	shifted := ""
+
 	for i := 0; i < len(s); i++ {
-		shifted[i] = byte(int(s[i]) + step)
+		shifted = shifted + string(s[i] + byte(step))
 	}
 
-	return string(shifted)
+	return shifted
 }
-
 // END
