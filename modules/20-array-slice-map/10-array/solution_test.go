49d3ce7f826faa36a280696106e5d4c4b54a22d7
Vladislav Kramorenko <85196001+Xewus@users.noreply.github.com>
2023-01-15 14:06:45 +0300

    Update solution_test.go
    В существующих тестах не проверяются граничные условия, из-за чего проходит неверное решение, например:
    
    `if i < len(nums) && i > 0 `

--- modules/20-array-slice-map/10-array/solution_test.go
+++ modules/20-array-slice-map/10-array/solution_test.go
@@ -12,6 +12,8 @@ func TestSafeWrite(t *testing.T) {
 	a.Equal([5]int{}, SafeWrite([5]int{}, -10, 10))
 	a.Equal([5]int{}, SafeWrite([5]int{}, 10, 10))
 	a.Equal([5]int{}, SafeWrite([5]int{}, 20, 10))
+	a.Equal([5]int{}, SafeWrite([5]int{}, 5, 10))
+	a.Equal([5]int{10, 2, 10, 4, 5}, SafeWrite([5]int{1, 2, 3, 4, 5}, 0, 10))
 	a.Equal([5]int{1, 2, 10, 4, 5}, SafeWrite([5]int{1, 2, 3, 4, 5}, 2, 10))
 	a.Equal([5]int{1, 2, 3, 4, 22}, SafeWrite([5]int{1, 2, 3, 4, 5}, 4, 22))
 }
