6a705be55d0f0d490f4a7af47189341662411e12
Elena Gromova <elena.gromova@hexlet.io>
2024-01-23 15:56:15 +0300

    fixed tests

--- modules/20-array-slice-map/40-map-for/solution_test.go
+++ modules/20-array-slice-map/40-map-for/solution_test.go
@@ -12,4 +12,5 @@ func TestMostPopularWord(t *testing.T) {
 	a.Equal("world", MostPopularWord([]string{"hello", "world", "hello", "world", "world"}))
 	a.Equal("one", MostPopularWord([]string{"one", "two", "three", "four", "five"}))
 	a.Equal("c", MostPopularWord([]string{"a", "b", "c", "c", "d", "e", "e", "d"}))
+	a.Equal("a", MostPopularWord([]string{"a", "b", "b", "a"}))
 }
