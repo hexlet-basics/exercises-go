1044b9b7df47af95416d61fc11c2ad02f705a8f3
Kirill Mokevnin <mokevnin@gmail.com>
2025-07-13 08:05:34 -0500

    fix tests
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/10-basics/35-math/solution_test.go
+++ modules/10-basics/35-math/solution_test.go
@@ -9,9 +9,9 @@ import (
 func TestCalculateProgress(t *testing.T) {
 	a := assert.New(t)
 
-	a.InEpsilon(0.0, CalculateProgress(0, 10), 0.0001)
-	a.InEpsilon(0.25, CalculateProgress(1, 4), 0.0001)
-	a.InEpsilon(0.4, CalculateProgress(2, 5), 0.0001)
-	a.InEpsilon(0.75, CalculateProgress(3, 4), 0.0001)
-	a.InEpsilon(1.0, CalculateProgress(5, 5), 0.0001)
+	a.InDelta(0.0, CalculateProgress(0, 10), 0.0001)
+	a.InDelta(0.25, CalculateProgress(1, 4), 0.0001)
+	a.InDelta(0.4, CalculateProgress(2, 5), 0.0001)
+	a.InDelta(0.75, CalculateProgress(3, 4), 0.0001)
+	a.InDelta(1.0, CalculateProgress(5, 5), 0.0001)
 }
