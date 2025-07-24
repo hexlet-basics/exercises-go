553f742e6900979b25588ee639096d69af7f1e70
Kirill Mokevnin <mokevnin@gmail.com>
2025-07-24 14:38:11 -0400

    fix tests
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- modules/10-basics/25-variables/solution_test.go
+++ modules/10-basics/25-variables/solution_test.go
@@ -12,16 +12,21 @@ func TestPrintBody(t *testing.T) {
 	// Создаём буфер и перенаправляем os.Stdout
 	var buf bytes.Buffer
 	old := os.Stdout
-	r, w, _ := os.Pipe()
+	r, w, err := os.Pipe()
+	assert.NoError(t, err)
+
 	os.Stdout = w
 
 	// Вызываем тестируемую функцию
 	PrintBody()
 
 	// Закрываем запись и читаем вывод
-	w.Close()
+	err = w.Close()
+	assert.NoError(t, err)
 	os.Stdout = old
-	_, _ = buf.ReadFrom(r)
+
+	_, err = buf.ReadFrom(r)
+	assert.NoError(t, err)
 
 	expected := "Go Go 1.21\n"
 	assert.Equal(t, expected, buf.String(), "Вывод функции PrintBody не совпадает с ожидаемым")
