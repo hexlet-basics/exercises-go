449d7f369bf5c943f771ccab126a5a37686afd08
Nikolay Gagarinov <feycot@gmail.com>
2025-07-25 10:33:02 +0500

    fix with comments

--- modules/20-slices/100-arrays/solution.go
+++ modules/20-slices/100-arrays/solution.go
@@ -2,7 +2,6 @@ package solution
 
 // BEGIN
 
-// SafeWrite writes a value val in the array nums if the index i is in the array's boundaries.
 func SafeWrite(nums [5]int, i, val int) [5]int {
 	if i >= 0 && i < len(nums) {
 		nums[i] = val
